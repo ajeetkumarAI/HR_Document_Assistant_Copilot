440d83e51ca1ca6238f2bccafda9520419e573b0
ajeetkumarAI <ajeetkumar.kumar.4434@gmail.com>
2026-05-07 08:49:39 +0530

    Add OpenAI API key integration for embedding and LLM models

--- src/embeddings.py
+++ src/embeddings.py
@@ -5,5 +5,5 @@ import os
 load_dotenv()
 
 def get_embedding_model():
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small",openai_api_key=os.getenv("OPENAI_API_KEY"))
     return embeddings
--- src/llm_integration.py
+++ src/llm_integration.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 def get_llm_model():
-    llm = ChatOpenAI(model_name="gpt-5.4-nano-2026-03-17", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-5.4-nano-2026-03-17", temperature=0,openai_api_key=os.getenv("OPENAI_API_KEY"))
     return llm
 
